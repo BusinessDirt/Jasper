543e47450f18cf47b572b50fcc81222bf5e714f2
Maximilian Bollschweiler <maxi.bollschweiler@gmail.com>
2025-11-13 12:04:28 +0100

    feat(events): add implementation for the EventListener

--- src/main/java/github/businessdirt/jasper/events/system/EventListener.java
+++ src/main/java/github/businessdirt/jasper/events/system/EventListener.java
@@ -1,4 +1,36 @@
 package github.businessdirt.jasper.events.system;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 public class EventListener {
+    public final String name;
+    public final Consumer<Object> invoker;
+    public final HandleEvent.Priority priority;
+    public final boolean canReceiveCancelled;
+
+    private final List<Predicate<Event>> predicates;
+
+    public EventListener(
+            String name,
+            Consumer<Object> invoker,
+            HandleEvent options,
+            List<Predicate<Event>> extraPredicates
+    ) {
+        this.name = name;
+        this.invoker = invoker;
+        this.priority = options.priority();
+        this.canReceiveCancelled = options.receiveCancelled();
+
+        this.predicates = new ArrayList<>();
+        if (!canReceiveCancelled) this.predicates.add(event -> !event.isCancelled());
+
+        this.predicates.addAll(extraPredicates);
+    }
+
+    public boolean shouldInvoke(Event event) {
+        return predicates.stream().allMatch(predicate -> predicate.test(event));
+    }
 }

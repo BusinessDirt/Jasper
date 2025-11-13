6cc7b235cb2fe2573b0a6deadc2134988d541ef3
Maximilian Bollschweiler <maxi.bollschweiler@gmail.com>
2025-11-13 11:57:28 +0100

    feat(events): add implementation for Event and add a CancellableEvent

---
+++ src/main/java/github/businessdirt/jasper/events/system/CancellableEvent.java
@@ -0,0 +1,4 @@
+package github.businessdirt.jasper.events.system;
+
+public abstract class CancellableEvent extends Event implements Event.Cancellable {
+}
--- src/main/java/github/businessdirt/jasper/events/system/Event.java
+++ src/main/java/github/businessdirt/jasper/events/system/Event.java
@@ -1,4 +1,30 @@
 package github.businessdirt.jasper.events.system;
 
+import java.util.function.Consumer;
+
 public abstract class Event {
+
+    private boolean isCancelled = false;
+
+    public boolean isCancelled() {
+        return isCancelled;
+    }
+
+    private void setCancelled(boolean cancelled) {
+        this.isCancelled = cancelled;
+    }
+
+    public boolean post() {
+        return this.post(null);
+    }
+
+    public boolean post(Consumer<Throwable> onError) {
+        return EventBus.getEventHandler(this.getClass()).post(this, onError);
+    }
+
+    public interface Cancellable {
+        default void cancel() {
+            ((Event) this).setCancelled(true);
+        }
+    }
 }

f10b4281bee1d7f36031ff6dc46bb3ff52bcc5f7
Maximilian Bollschweiler <maxi.bollschweiler@gmail.com>
2025-11-14 11:10:50 +0100

    feat(events): automatically post ApplicationShutdownEvent when the program terminates

--- src/main/java/github/businessdirt/jasper/events/events/application/ApplicationStartEvent.java
+++ src/main/java/github/businessdirt/jasper/events/events/application/ApplicationStartEvent.java
@@ -2,4 +2,11 @@ package github.businessdirt.jasper.events.events.application;
 
 import github.businessdirt.jasper.events.system.Event;
 
-public class ApplicationStartEvent extends Event { }
+public class ApplicationStartEvent extends Event {
+
+    public ApplicationStartEvent() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            new ApplicationShutdownEvent().post();
+        }));
+    }
+}

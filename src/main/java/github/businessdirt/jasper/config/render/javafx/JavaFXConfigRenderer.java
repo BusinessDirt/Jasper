91d8644b3a91f889271f29c28ddd517fab5c31db
Maximilian Bollschweiler <maxi.bollschweiler@gmail.com>
2025-11-15 17:24:34 +0100

    feat(config): add JavaFXConfigRenderer

---
+++ src/main/java/github/businessdirt/jasper/config/render/javafx/JavaFXConfigRenderer.java
@@ -0,0 +1,12 @@
+package github.businessdirt.jasper.config.render.javafx;
+
+import github.businessdirt.jasper.config.ConfigHandler;
+import github.businessdirt.jasper.config.render.ConfigRenderer;
+
+public class JavaFXConfigRenderer<C extends ConfigHandler> extends ConfigRenderer<C, Object> {
+
+    @Override
+    public Object render(C config, String searchQuery) {
+        return null;
+    }
+}
